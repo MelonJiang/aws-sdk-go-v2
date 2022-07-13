24d87b63d08e648ebe64518ff34298d9285d33e8
realotz <78065238+realotz@users.noreply.github.com>
2022-07-13 09:33:54 +0800

    Update types.go

--- service/s3/types/types.go
+++ service/s3/types/types.go
@@ -497,6 +497,7 @@ type CreateBucketConfiguration struct {
 	// Specifies the Region where the bucket will be created. If you don't specify a
 	// Region, the bucket is created in the US East (N. Virginia) Region (us-east-1).
 	LocationConstraint BucketLocationConstraint
+	StorageClass *string
 
 	noSmithyDocumentSerde
 }
